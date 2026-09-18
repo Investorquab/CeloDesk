649d65ae71a82c92ab351de7660c25d5606fdd07
quabnation <quadribello3@gmail.com>
2026-09-18 11:01:36 +0100

    Fix Remotion composition registration

--- demo-video/src/index.ts
+++ demo-video/src/index.ts
@@ -1,4 +1,16 @@
-import { registerRoot } from "remotion";
+import React from "react";
+import { Composition, registerRoot } from "remotion";
 import { CeloDeskVideo } from "./CeloDeskVideo";
 
-registerRoot(() => <CeloDeskVideo />);
+const RemotionRoot: React.FC = () => (
+  <Composition
+    id="CeloDeskDemo"
+    component={CeloDeskVideo}
+    durationInFrames={1890}
+    fps={30}
+    width={1920}
+    height={1080}
+  />
+);
+
+registerRoot(RemotionRoot);
